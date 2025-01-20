b50ae6fcc73048e79ab247c73e9888690bf79b48
Parishmaa <hparishma22@tbc.edu.np>
2025-01-20 12:27:30 +0545

    added factories

---
+++ app/Models/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'), // Default password
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
